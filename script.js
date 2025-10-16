6b4c6a3c30644b520f63524408d188439915c059
Kevin Bayley <kevin.p.bayley@gmail.com>
2025-10-15 20:21:44 -0700

    Re-organize arithmatic functions

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-let add = function(x, y){
-    return x+y;
-}
-
-let subtract = function(x, y){
-    return x-y;
-}
-
-let multiply = function(x, y){
-    return x*y;
-}
-
-let divide = function(x, y){
-    return x / y;
-}
+let add = function(x, y){return x+y;}
+let subtract = function(x, y){return x-y;}
+let multiply = function(x, y){return x*y;}
+let divide = function(x, y){return x / y;}
