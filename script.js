f49a5fdc0e07bdc3ace45a6384f667dd679ced2c
Kevin Bayley <kevin.p.bayley@gmail.com>
2025-10-16 16:23:12 -0700

    Add operator function to initiate math

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ let divide = function(x, y){return x / y;}
 let num1 = 0;
 let num2 = 0;
 let operator = "";
+
+let operate = function(op, n1, n2){
+    if (op === "+") add(n1, n2);
+    if (op === "-") subtract(n1, n2);
+    if (op === "*") multiply(n1, n2);
+    if (op === "/") divide(n1, n2);
+}
