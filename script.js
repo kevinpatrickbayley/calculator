7b7eaabb860cdef4974879f40515d2af42e7810f
Kevin Bayley <kevin.p.bayley@gmail.com>
2025-10-16 16:17:29 -0700

    Add arithmetic variables, 2 nums and an operator

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let add = function(x, y){return x+y;}
 let subtract = function(x, y){return x-y;}
 let multiply = function(x, y){return x*y;}
 let divide = function(x, y){return x / y;}
+
+let num1 = 0;
+let num2 = 0;
+let operator = "";
