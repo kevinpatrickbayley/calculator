d64e859497582c08591a313ea1fffcf933865fcc
Kevin Bayley <kevin.p.bayley@gmail.com>
2025-10-17 12:03:27 -0700

    Update operator regex to include floating points without a leading digit

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 let add = s => {
-    const rx = /(-?\d+(?:\.\d+)?)\+(-?\d+(?:\.\d+)?)/;
+    const rx = /((?:\d*\.\d+|\d+)?)\+((?:\d*\.\d+|\d+)?)/;
     while (rx.test(s)) s = s.replace(rx, (_, a, b) => String(parseFloat(a) + parseFloat(b)));
     return s;
 };
 
 let subtract = s => {
-    const rx = /(-?\d+(?:\.\d+)?)\-(-?\d+(?:\.\d+)?)/;
+    const rx = /((?:\d*\.\d+|\d+)?)\-((?:\d*\.\d+|\d+)?)/;
     while (rx.test(s)) s = s.replace(rx, (_, a, b) => String(parseFloat(a) - parseFloat(b)));
     return s;
 };
 
 const multiply = s => {
-    const rx = /(-?\d+(?:\.\d+)?)\*(-?\d+(?:\.\d+)?)/;
+    const rx = /((?:\d*\.\d+|\d+)?)\*((?:\d*\.\d+|\d+)?)/;
     while (rx.test(s)) s = s.replace(rx, (_, a, b) => String(parseFloat(a) * parseFloat(b)));
     return s;
 };
 
 let divide = s => {
-    const rx = /(-?\d+(?:\.\d+)?)\/(-?\d+(?:\.\d+)?)/;
+    const rx = /((?:\d*\.\d+|\d+)?)\/((?:\d*\.\d+|\d+)?)/;
     while (rx.test(s)) s = s.replace(rx, (_, a, b) => String(parseFloat(a) / parseFloat(b)));
     return s;
 };
@@ -46,6 +46,6 @@ keypadGrid.addEventListener("click", function(event){
     //equal key
     if (event.target && event.target.classList.contains("key-equal")){
         //order of operations
-        tempDisplayTest = add(divide(multiply(tempDisplayTest)));
-        displayText.textContent = tempDisplayTest;
+        tempDisplayTest = subtract(add(divide(multiply(tempDisplayTest))));
+        displayText.textContent = tempDisplayTest.slice(0,11);
     }});
