46289a0fbce80d2f2e7bc2201f77bacea561b294
Kevin Bayley <kevin.p.bayley@gmail.com>
2025-10-17 11:31:12 -0700

    Add multiplication functionality

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let add = function(x, y){return x+y;}
 let subtract = function(x, y){return x-y;}
-let multiply = function(x, y){return x*y;}
+const multiply = s => {
+  const rx = /(-?\d+(?:\.\d+)?)\*(-?\d+(?:\.\d+)?)/;
+  while (rx.test(s)) s = s.replace(rx, (_, a, b) => String(parseFloat(a) * parseFloat(b)));
+  return s;
+};
 let divide = function(x, y){return x / y;}
 
 let num1 = 0;
@@ -21,7 +25,7 @@ const keypadGrid = document.getElementById("keypad-grid");
 
 keypadGrid.addEventListener("click", function(event){
     //numbers and operators
-    if (event.target && event.target.classList.contains("key-print") && tempDisplayTest.length < 14){
+    if (event.target && event.target.classList.contains("key-print") && tempDisplayTest.length < 11){
         tempDisplayTest += event.target.textContent;
         displayText.textContent = tempDisplayTest;
     }
@@ -35,4 +39,8 @@ keypadGrid.addEventListener("click", function(event){
         tempDisplayTest = tempDisplayTest.slice(0,-1);
         displayText.textContent = tempDisplayTest;
     }
-})
+    //equal key
+    if (event.target && event.target.classList.contains("key-equal")){
+        tempDisplayTest = multiply(tempDisplayTest);
+        displayText.textContent = tempDisplayTest;
+    }})
